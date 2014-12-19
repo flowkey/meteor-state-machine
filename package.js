2659881b4f5e3e6367aff6b167b25175653c0a79
Ahmed Hassan <a.hassan@buismed.de>
2014-12-19 14:38:29 +0100

    added tracker dependency

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "Reactive Javascript-state-machine for Meteor",
-  version: "0.1.0",
+  version: "0.1.1",
   git: "https://github.com/zhouzhuojie/meteor-state-machine.git",
   name: "flowkey:state-machine"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom("1.0")
-  api.use("reactive-var");
+  api.use("tracker");
   api.add_files('state-machine.js', 'client');
   api.add_files('main.js', 'client');
   if (api.export){
